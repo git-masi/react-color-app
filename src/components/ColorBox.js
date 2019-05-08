69e153b0073571eb9ab7ea48d4838bc953722869
Eric Masi <eric@gitmasi.com>
2019-05-08 11:04:00 -0400

    add click handler, future route to more colors

--- src/components/ColorBox.js
+++ src/components/ColorBox.js
@@ -1,35 +1,37 @@
 import React, { Component } from 'react';
 import './ColorBox.css';
 import CopyOverlay from './CopyOverlay';
+import { withRouter } from 'react-router-dom';
   
 class ColorBox extends Component {
   state = {
     copied: false
   }
 
-  copyHandler = () =>{
+  copyHandler = () => {
     navigator.clipboard.writeText(this.props.background);
-    // console.log(this.state.copied);
     this.setState({copied: true}, () => setTimeout(() => this.setState({copied: false}), 1500));
+  }
+
+  moreClickedHandler = () => {
+    this.props.history.push('/');
   }
 
   render() {
     const { name, background } = this.props;
-    // const overlay = this.state.copied ? <CopyOverlay color={background} show={this.state.copied}/> : null;
     return (
       <div className="ColorBox" style={{background: background}}>
         <div className='copy-container'>
-          {/* {overlay} */}
           <CopyOverlay color={background} show={this.state.copied}/>
           <div className='box-content'>
             <span>{name}</span>
           </div>
           <button className='copy-button' onClick={this.copyHandler}>Copy</button>
         </div>
-        <span className='see-more'>More</span>
+        <span className='see-more' onClick={this.moreClickedHandler}>More</span>
       </div>
     )
   }
 }
 
-export default ColorBox;
+export default withRouter(ColorBox);
