886441dbeaa286125083ec98978d01b7195c3866
Eric Masi <eric@gitmasi.com>
2019-05-16 12:28:58 -0400

    refect ColorBox styles to JSS

--- src/components/ColorBox.js
+++ src/components/ColorBox.js
@@ -1,8 +1,76 @@
 import React, { Component } from 'react';
-import './ColorBox.css';
 import CopyOverlay from './CopyOverlay';
 import { withRouter, Link } from 'react-router-dom';
 import chroma from 'chroma-js';
+import { withStyles } from '@material-ui/styles';
+import classNames from 'classnames';
+
+const styles = {
+  colorBox: {
+    position: 'relative',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+
+    '&:hover $copyButton': {
+      opacity: 1,
+      transition: '0.5s',
+    },
+  },
+  
+  copyButton: {
+    width: '100px',
+    height: '25px',
+    display: 'inline-block',
+  
+    fontSize: '1rem',
+    lineHeight: '25px',
+    textAlign: 'center',
+    
+    outline: 'none',
+    background: 'rgba(255, 255, 255, 0.3)',
+    color: 'white',
+    border: 'none',
+    opacity: 0,
+    
+    cursor: 'pointer',
+    textTransform: 'uppercase',
+  },
+  
+  boxContent: {
+    position: 'absolute',
+    left: '0px',
+    bottom: '0px',
+    padding: '3px 10px',
+    color: 'black',
+    letterSpacing: '1px',
+    textTransform: 'uppercase',
+    fontSize: '12px',
+  },
+  
+  seeMore: {
+    background: 'rgba(255, 255, 255, 0.3)',
+    position: 'absolute',
+    border: 'none',
+    right: '0px',
+    bottom: '0px',
+    color: 'white',
+    width: '60px',
+    height: '25px',
+    textAlign: 'center',
+    lineHeight: '25px',
+    textTransform: 'uppercase',
+    cursor: 'pointer',
+  },
+  
+  darkColor: {
+    color: 'white',
+  },
+  
+  lightColor: {
+    background: 'rgba(0, 0, 0, 0.7)',
+  },
+}
   
 class ColorBox extends Component {
   state = {
@@ -15,21 +83,44 @@ class ColorBox extends Component {
   }
   
   render() {
-    const { name, paletteName, background, id } = this.props;
+    const { name, paletteName, background, id, classes } = this.props;
     const isDarkColor = chroma(background).luminance() <= 0.08;
     const isLightColor = chroma(background).luminance() >= 0.55;
 
     return (
-      <div className="ColorBox" style={{background: background}}>
+      <div className={classes.colorBox} style={{background: background}}>
         <CopyOverlay color={background} show={this.state.copied} isdark={isDarkColor} islight={isLightColor}/>
-        <div className='box-content'>
-          <span className={isDarkColor ? 'dark-color' : null}>{name}</span>
+        <div className={classes.boxContent}>
+          <span
+            className={classNames({
+              [classes.darkColor]: isDarkColor,
+            })}
+          >
+            {name}
+          </span>
         </div>
-        <button className={`copy-button ${isLightColor ? 'light-color' : null}`} onClick={this.copyHandler}>Copy</button>
-        {this.props.singleColor ? null : <Link className={`see-more ${isLightColor ? 'light-color' : null}`} to={`/palette/${paletteName}/${id}`}>More</Link>}
+        <button
+          className={classNames(classes.copyButton, {
+            [classes.lightColor]: isLightColor,
+          })}
+          onClick={this.copyHandler}
+        >
+          Copy
+        </button>
+        {this.props.singleColor ?
+          null :
+          <Link
+            className={classNames(classes.seeMore, {
+              [classes.lightColor]: isLightColor,
+            })}
+            to={`/palette/${paletteName}/${id}`}
+          >
+            More
+          </Link>
+        }
       </div>
     )
   }
 }
 
-export default withRouter(ColorBox);
+export default withRouter(withStyles(styles)(ColorBox));
